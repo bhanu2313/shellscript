7d2b7140e903c9b0dab6606b05824da51c096765
bhanu2313 <bhanu.vnvb@gmail.com>
2024-04-29 12:39:38 +0530

    scripts

---
+++ script03.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+echo "A valu"
+read a
+echo "B valu"
+read b
+c=`expr $a + $b`
+echo "c = $c"
+
---
+++ script04.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+echo "A valu"
+a=$1
+echo "B valu"
+b=$2
+c=`expr $a + $b`
+echo "c = $c"
+
